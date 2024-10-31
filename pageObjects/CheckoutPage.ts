4e4ae5de1e9d8154c14fd145fec6f0eb5956b978
Hleb Matsiash <hmatsiash@gmail.com>
2024-10-31 23:38:37 +0100

    EPP-3: checkout e2e test

--- pageObjects/CheckoutPage.ts
+++ pageObjects/CheckoutPage.ts
@@ -11,12 +11,19 @@ export class CheckoutPage {
 
     async selectGuestCheckout() {
         await this.page.click(selectors.checkoutPage.guestCheckoutRadio);
-        await this.page.click(selectors.checkoutPage.continueButton);
+        await this.page.click(selectors.checkoutPage.continueAccountButton);
+    }
+
+    async selectPaymentMethod() {
+        await this.page.click(selectors.checkoutPage.termAndConditionsCheckbox);
+        await this.page.click(selectors.checkoutPage.paymentMethodButton);
     }
 
     async fillAddressForm(addressData: {
         firstName: string,
         lastName: string,
+        email: string,
+        phone: string,
         address: string,
         city: string,
         postalCode: string,
@@ -25,11 +32,13 @@ export class CheckoutPage {
     }) {
         await this.page.fill(selectors.checkoutPage.firstNameInput, addressData.firstName);
         await this.page.fill(selectors.checkoutPage.lastNameInput, addressData.lastName);
+        await this.page.fill(selectors.checkoutPage.lastNameInput, addressData.email);
+        await this.page.fill(selectors.checkoutPage.lastNameInput, addressData.phone);
         await this.page.fill(selectors.checkoutPage.addressInput, addressData.address);
         await this.page.fill(selectors.checkoutPage.cityInput, addressData.city);
         await this.page.fill(selectors.checkoutPage.postalCodeInput, addressData.postalCode);
         await this.page.selectOption(selectors.checkoutPage.countrySelect, addressData.country);
         await this.page.selectOption(selectors.checkoutPage.regionSelect, addressData.region);
-        await this.page.click(selectors.checkoutPage.continueButton);
+        await this.page.click(selectors.checkoutPage.continueBillingButton);
     }
 }
