a3af4ab32aad2e9b647b0cd07fea7e0156b6687f
Hleb Matsiash <hmatsiash@gmail.com>
2024-11-04 23:31:16 +0100

    EPP-4: add filtering test

--- tests/filters-and-sorting.spec.ts
+++ tests/filters-and-sorting.spec.ts
@@ -6,10 +6,7 @@ test('Filter by category: Verify only relevant products are displayed', async ({
     const homePage = new HomePage(page);
     const filtersPage = new FiltersPage(page);
 
-    // Step 1: Navigate to the homepage
     await homePage.navigateTo('/');
-
-    // Step 2: Navigate to the category section (e.g., Electronics)
     await filtersPage.navigateToCategory('Electronics');
 
     // Step 3: Apply filter by specific subcategory (e.g., Smartphones)
