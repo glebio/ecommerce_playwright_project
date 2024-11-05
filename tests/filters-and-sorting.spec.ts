932649b29c334a87d9c2e17a20d9f7c48ca2ebec
Hleb Matsiash <hmatsiash@gmail.com>
2024-11-05 23:49:35 +0100

    EPP-4: choose category for filtering test

--- tests/filters-and-sorting.spec.ts
+++ tests/filters-and-sorting.spec.ts
@@ -7,11 +7,11 @@ test('Filter by category: Verify only relevant products are displayed', async ({
     const filtersPage = new FiltersPage(page);
 
     await homePage.navigateTo('/');
-    await filtersPage.navigateToCategory('Electronics');
+    await filtersPage.navigateToCategory('Mobile & Accessories');
 
     // Step 3: Apply filter by specific subcategory (e.g., Smartphones)
-    await filtersPage.applyCategoryFilter('Category', 'Smartphones');
+    await filtersPage.applyCategoryFilter('Category', 'Mobile Phones');
 
     // Step 4: Verify that the products displayed belong to the selected category
-    await filtersPage.verifyFilteredProducts(['Smartphone']);
+    await filtersPage.verifyFilteredProducts(['Mobile Phones']);
 });
