cf6538f4212b8f0751654f795bbf4e476d83f3c0
Hleb Matsiash <hmatsiash@gmail.com>
2024-12-09 20:06:01 +0100

    EPP-8: fix autotests for new UI

--- pageObjects/OrderSuccessPage.ts
+++ pageObjects/OrderSuccessPage.ts
@@ -8,6 +8,6 @@ export class OrderSuccessPage {
     async verifyOrderSuccessMessage() {
         const successMessage = this.page.locator(selectors.orderSuccessPage.successMessage);
         await expect(successMessage).toBeVisible();
-        await expect(successMessage).toContainText('Your order has been placed!');
+        await expect(successMessage).toContainText('Your order has been successfully processed!');
     }
 }
