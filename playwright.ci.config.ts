e107d454e945775da4cc60b41fc42244e6739c2e
Hleb Matsiash <hmatsiash@gmail.com>
2026-02-07 12:41:03 +0100

    EPP-106: fix 2: add CI smoke test to check CI process

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -12,5 +12,8 @@ export default defineConfig({
     projects: [
         {name: 'chromium', use: {browserName: 'chromium', viewport: null}},
     ],
-    reporter: [['allure-playwright']],
+    reporter: [
+        ['html', {open: 'never'}],
+        ['allure-playwright']
+    ],
 });
