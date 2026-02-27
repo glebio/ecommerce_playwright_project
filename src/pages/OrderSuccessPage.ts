18bd6081ca06c09b8745acc0fcac176e694bdb15
Hleb Matsiash <hmatsiash@gmail.com>
2026-02-27 13:38:23 +0100

    chore: reorganize project structure, separate ci-smoke and legacy files

--- src/pages/OrderSuccessPage.ts
+++ src/pages/OrderSuccessPage.ts
@@ -1,13 +1,1 @@
-import {Page, expect} from '@playwright/test';
-import {selectors} from '../selectors/selectors';
-
-export class OrderSuccessPage {
-    constructor(private page: Page) {
-    }
-
-    async verifyOrderSuccessMessage() {
-        const successMessage = this.page.locator(selectors.orderSuccessPage.successMessage);
-        await expect(successMessage).toBeVisible();
-        await expect(successMessage).toContainText('Your order has been successfully processed!');
-    }
-}
+export * from './_legacy/OrderSuccessPage';
